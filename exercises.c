36196f8514133b2ff84e01518e5327431b2ab988
Your Name <you@example.com>
2022-03-13 18:07:40 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -100,7 +100,7 @@ la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
 
-   v -> datos[i] = valor;
+   
 
 }
 
@@ -110,7 +110,10 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-   return 0;
+
+   valor = v -> datos[i];
+
+   return valor;
 }
 
 /*
